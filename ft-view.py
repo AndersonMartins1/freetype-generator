bf3ffb5b5a5775048b9c637d72402623e3d7f1cf
Anderson Preza Martins <lordearena@gmail.com>
2024-03-09 14:06:35 +0100

    Update ft-view.py
    This should help structure your code better and make it more readable to other developers when viewed on GitHub.

--- ft-view.py
+++ ft-view.py
@@ -1,57 +1,60 @@
 #!/usr/bin/env python3
-# coding: utf8                
+# -*- coding: utf-8 -*-
 
-help = """FreeType View - get a FreeType font file and display it's bitmap representation (for all characters or a single char).
-This script is useful to get font sizing (in pixels) and its representation (under bitmap format).  
- 
+"""
+FreeType View - Get a FreeType font file and display its bitmap representation (for all characters or a single char).
+This script is useful to get font sizing (in pixels) and its representation (under bitmap format).
+
 Usage:
   ft-view.py <ttf_file> <ttf_size> [--char=<char>] [--descenders=<comma-separated-list>] [--special-align=<comma-separated-list>]
- 
+
 Options:
-  -h --help          This helps screen
+  -h --help          Display this help screen
 
 Examples:
-  python3 ft-view.py ttf-fonts/Vera.ttf 13           --> Will displays all the characters
-  python3 ft-view.py ttf-fonts/Vera.ttf 13 --char=A  --> Display only the char "A"
-  python3 ft-view.py tft-fonts/heydings_icons.ttf 23 --char=A --descenders=   --> ignore all descenders
-  python3  ft-view.py ttf-fonts/Vera.ttf 13 --descenders=p,q,j,#34 --> change the descenders
-  python3  ft-view.py ttf-fonts/Vera.ttf 13 --descenders=          --> No descenders so baseline = bottomline! 
-  python3  ft-view.py ttf-fonts/Vera.ttf 13 --special-align=a:T,=:B,#126:M --> change the special alignment instruction. a on TOP, = on Bottom, ~(#126) in middle (between the baseline and top)
-  python3  ft-view.py ttf-fonts/Vera.ttf 13 --special-align=       --> delete special alignments. All chars on Bottom line (ideal for Wingdings chars).
+  python3 ft-view.py ttf-fonts/Vera.ttf 13           # Will display all the characters
+  python3 ft-view.py ttf-fonts/Vera.ttf 13 --char=A  # Display only the char "A"
+  python3 ft-view.py tft-fonts/heydings_icons.ttf 23 --char=A --descenders=   # Ignore all descenders
+  python3  ft-view.py ttf-fonts/Vera.ttf 13 --descenders=p,q,j,#34  # Change the descenders
+  python3  ft-view.py ttf-fonts/Vera.ttf 13 --descenders=            # No descenders, so baseline = bottomline! 
+  python3  ft-view.py ttf-fonts/Vera.ttf 13 --special-align=a:T,=:B,#126:M  # Change the special alignment instruction
+  python3  ft-view.py ttf-fonts/Vera.ttf 13 --special-align=       # Delete special alignments. All chars on bottom line (ideal for Wingdings chars).
 
 Happy Electronic Hacking. 
 MCHobby.be
-""" 
+"""
 
 # FreeType generator common items
 from ftcommon import *
 from docopt import docopt
 import sys
 
-if __name__ == '__main__':
-    arguments = docopt( help )
+def main():
+    arguments = docopt(__doc__)
 
-    char_filter = arguments['--char'] # eg: 'B' or None
+    char_filter = arguments['--char']  # eg: 'B' or None
+
+    print(f"Load font {arguments['<ttf_file>']}, set size to {int(arguments['<ttf_size>'])}")
+
+    font_loader = FreeTypeLoader(font_file=arguments['<ttf_file>'], font_size=int(arguments['<ttf_size>']))
     
-    print( "Load font %s, set size to %i" % (arguments['<ttf_file>'], int(arguments['<ttf_size>'])) )             
-    font_loader = FreeTypeLoader( font_file=arguments['<ttf_file>'], font_size=int(arguments['<ttf_size>']) )
-    if arguments['--descenders'] != None: # redefine the default descenders
-       font_loader.set_descenders( arguments['--descenders'] ) 
-    if arguments['--special-align' ] != None: # redefine the default special alignments
-       font_loader.set_special_align( arguments['--special-align' ] )  
+    if arguments['--descenders'] is not None:  # redefine the default descenders
+        font_loader.set_descenders(arguments['--descenders'])
 
-    print( "max size (width,height): %i,%i" %(font_loader.max_width, font_loader.max_height ) )
-    print( 'descender size = %i' % font_loader.descender_size )
+    if arguments['--special-align'] is not None:  # redefine the default special alignments
+        font_loader.set_special_align(arguments['--special-align'])
 
+    print(f"Max size (width,height): {font_loader.max_width},{font_loader.max_height}")
+    print(f'Descender size = {font_loader.descender_size}')
 
-    #  for ordinal in font_loader.characters.keys():
     for ordinal in font_loader.char_ordinals:
-        if (char_filter != None) and (ord(char_filter) != ordinal ):
+        if char_filter is not None and ord(char_filter) != ordinal:
             continue
 
         # print the bitmap representation of a character
-        #   include separator & dimension 
-        font_loader.print_character( ordinal )
+        #   include separator & dimension
+        font_loader.print_character(ordinal)
 
-
+if __name__ == '__main__':
+    main()
 
